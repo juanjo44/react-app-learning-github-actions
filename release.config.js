74760e89d9edcf8f3c70a40ed8da1f7e1687acfe
Juan Betancourt Osorio <juan.betancourto@perficient.com>
2023-01-30 16:16:56 -0500

    feat: finally fixed? with prettier :cccc

--- release.config.js
+++ release.config.js
@@ -1,8 +1,5 @@
 module.exports = {
   branches: "master",
   repositoryUrl: "https://github.com/juanjo44/react-app-learning-github-actions",
-  plugins: [
-    "@semantic-release/commit-analyzer",
-    "@semantic-release/release-notes-generator"
-  ]
+  plugins: ["@semantic-release/commit-analyzer", "@semantic-release/release-notes-generator"]
 };
