a3a973b89d94018a528267d55eeef788020e4ea5
Juan Betancourt Osorio <juan.betancourto@perficient.com>
2023-01-30 15:20:38 -0500

    fix: fix description

---
+++ release.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  branches: "master",
+  repositoryUrl: "https://github.com/juanjo44/react-app-learning-github-actions",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/npm",
+    "@semantic-release/github"
+  ]
+}
