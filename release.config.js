e4e883347b712ec716967a7c6c826e9f4d0f72d1
Juan Betancourt Osorio <juan.betancourto@perficient.com>
2023-01-31 15:52:22 -0500

    I'm really really tired xD

--- release.config.js
+++ release.config.js
@@ -1,8 +1,5 @@
 module.exports = {
   branches: ["master"],
-  plugins: [
-    "@semantic-release/commit-analyzer",
-    "@semantic-release/release-notes-generator"
-  ],
+  plugins: ["@semantic-release/commit-analyzer", "@semantic-release/release-notes-generator"],
   repositoryUrl: "https://github.com/juanjo44/react-app-learning-github-actions"
 };
