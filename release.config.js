19c4ae5e5e9b6e8ddaf73251ad0b60ac305855b4
Juan Betancourt Osorio <juan.betancourto@perficient.com>
2023-01-31 16:33:19 -0500

    feat: El bien triunfa al final

--- release.config.js
+++ release.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   branches: ["master"],
-  plugins: ["@semantic-release/commit-analyzer", "@semantic-release/release-notes-generator"],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/github"
+  ],
   repositoryUrl: "https://github.com/juanjo44/react-app-learning-github-actions"
 };
