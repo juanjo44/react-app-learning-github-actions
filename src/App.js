538216c31f4cb0ca77f5d1e1c4b8d1cc301f0bbe
Juan Betancourt Osorio <juan.betancourto@perficient.com>
2023-01-31 10:57:05 -0500

    fetat: waiting for the win

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save your life from the enemies.
+          Edit <code>src/App.js</code> and save your life from the bad.
         </p>
         <a
           className="App-link"
