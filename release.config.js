f744841fc6d67c3f6313e3d9d88b72fe8fcdba11
Juan Betancourt Osorio <juan.betancourto@perficient.com>
2023-01-31 14:58:28 -0500

    feat: Final, if it doesn't work then i will comment and continue studying

--- release.config.js
+++ release.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   branches: "master",
-  repositoryUrl: "https://github.com/juanjo44/react-app-learning-github-actions",
+  repositoryUrl: "https://github.com/juanjo44/react-app-learning-github-actions.git",
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
