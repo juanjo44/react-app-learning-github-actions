952aa0eca4fe4a5806747b7711536eca0bda6611
Juan Betancourt Osorio <juan.betancourto@perficient.com>
2023-02-01 09:25:02 -0500

    fix: now we have assets xD

--- release.config.js
+++ release.config.js
@@ -3,7 +3,15 @@ module.exports = {
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
-    "@semantic-release/github"
+    [
+      "@semantic-release/github",
+      {
+        assets: [
+          { path: "build.zip", label: "Build" },
+          { path: "coverage.zip", label: "Coverage" }
+        ]
+      }
+    ]
   ],
   repositoryUrl: "https://github.com/juanjo44/react-app-learning-github-actions"
 };
