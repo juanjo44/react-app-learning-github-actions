c10129a3aa21de0569f486411eb747177b5688fd
Juan Betancourt Osorio <juan.betancourto@perficient.com>
2023-02-03 10:15:51 -0500

    feat: more about core
    More details in index.js about core

--- .github/actions/hello/index.js
+++ .github/actions/hello/index.js
@@ -3,13 +3,22 @@ const github = require("@actions/github");
 
 try {
   // throw(new Error("Some Error Message"));
+
+  core.debug("Debug message");
+  core.warning("Warning message");
+  core.error("Error message");
   const name = core.getInput("who-to-greet");
+  core.setSecret(name);
   console.log(`Hello ${name} welcome to Alien's House`);
 
   const time = new Date();
   core.setOutput("time", time.toTimeString());
 
+  core.startGroup("Logging github object");
   console.log(JSON.stringify(github, null, "\t"));
+  core.endGroup();
+
+  core.exportVariable("HELLO", "hello");
 } catch (error) {
   core.setFailed(error.message);
 }
